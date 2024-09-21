2e681c867de4dfd77695b48d00d648d39568f106
mohammadp1001 <mohammad.p69@gmail.com>
2024-09-21 08:38:00 +0000

    The new varilable time_column was added to s3_to_bigquery.py for checking data double ingesting.

--- flows/s3_to_bigquery.py
+++ flows/s3_to_bigquery.py
@@ -9,7 +9,7 @@ from typing import List, Optional
 from prefect import get_run_logger
 from google.cloud import bigquery
 from google.oauth2 import service_account
-from google.api_core.exceptions import BadRequest  
+from google.api_core.exceptions import BadRequest, NotFound
 
 if '/home/mohammad/RenewableInsight' not in sys.path:
     sys.path.append('/home/mohammad/RenewableInsight')
@@ -26,71 +26,109 @@ except ValidationError as e:
 def upload_parquet_to_bigquery(
     s3_key: str, 
     bigquery_table_id: str, 
+    time_column: str,
     expiration_time: Optional[int] = None
 ) -> None:
     """
-    Uploads a Parquet file from S3 to a BigQuery table.
+    Uploads a Parquet file from S3 to a BigQuery table, ensuring no duplicate data based on a time column.
 
-    
     :param s3_key: The S3 key for the Parquet file.
     :param bigquery_table_id: The BigQuery table ID.
+    :param time_column: The column name to determine data uniqueness.
     :param expiration_time: Expiration time in days for the BigQuery table.
     :return None
     """
     logger = get_run_logger()
+    
+    # Initialize S3 client
     s3 = boto3.client(
         's3', 
         aws_access_key_id=config.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
     )
-    response = s3.get_object(Bucket=config.BUCKET_NAME, Key=s3_key)
     
+    # Fetch the Parquet file from S3
+    response = s3.get_object(Bucket=config.BUCKET_NAME, Key=s3_key)
     parquet_file = BytesIO(response['Body'].read())
     df = pd.read_parquet(parquet_file)
-    schema = get_bq_schema_from_df(df)
-
-    expiration_ms = None
-    if expiration_time:
-        expiration_time = datetime.datetime.now(datetime.timezone.utc)  + datetime.timedelta(days=expiration_time)
-        expiration_ms = int(expiration_time.timestamp() * 1000)
-
+    
+    if time_column not in df.columns:
+        logger.error(f"The specified time_column '{time_column}' does not exist in the data.")
+        return
+    
+    # Initialize BigQuery client
     credentials = service_account.Credentials.from_service_account_file(config.GOOGLE_APPLICATION_CREDENTIALS)
     bigquery_client = bigquery.Client(credentials=credentials, project=config.PROJECT_ID)
     
-    project_id = config.BIGQUERY_PROJECT_ID
     dataset_id = config.BIGQUERY_DATASET_ID
     table_id = bigquery_table_id
-
-    dataset_ref = bigquery_client.dataset(dataset_id)
-    table_ref = dataset_ref.table(table_id)
-
+    table_ref = bigquery_client.dataset(dataset_id).table(table_id)
+    
+    # Check if the table exists; if not, create it
     try:
-        bigquery_client.get_table(table_ref)
-        logger.info("Table already exists.")
-    except Exception as e:
-        logger.info(f"Table does not exist. Creating table: {e}")
+        table = bigquery_client.get_table(table_ref)
+        logger.info(f"Table {bigquery_table_id} already exists.")
+    except NotFound:
+        logger.info(f"Table does not exist. Creating table {bigquery_table_id}.")
+        schema = get_bq_schema_from_df(df)
         table = bigquery.Table(table_ref, schema=schema)
-        if expiration_ms:
-            table.expires = expiration_time
+        
+        if expiration_time:
+            expiration_datetime = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(days=expiration_time)
+            table.expires = expiration_datetime
+        
         bigquery_client.create_table(table)
         logger.info(f"Table {bigquery_table_id} created successfully.")
     
-
-    job_config = bigquery.LoadJobConfig(
-            write_disposition=bigquery.WriteDisposition.WRITE_APPEND, 
-            schema=schema,
-            source_format=bigquery.SourceFormat.PARQUET,
-    )
+    # Retrieve the maximum value of the time_column from BigQuery
+    query = f"""
+        SELECT MAX({time_column}) as max_time
+        FROM `{config.BIGQUERY_PROJECT_ID}.{dataset_id}.{table_id}`
+    """
     try:
-        job = bigquery_client.load_table_from_dataframe(df, table_ref, job_config=job_config)
-        job.result()  
-        logger.info(f"Data successfully uploaded to {bigquery_table_id}")
+        query_job = bigquery_client.query(query)
+        result = query_job.result()
+        max_time = next(result).max_time
+        
+        if pd.isna(max_time):
+            logger.info("Table is empty. Uploading all data.")
+            filtered_df = df
+        else:
+            logger.info(f"Maximum {time_column} in BigQuery: {max_time}")
+            # Ensure the time_column is in datetime format
+            df[time_column] = pd.to_datetime(df[time_column])
+            if isinstance(max_time, str):
+                max_time = pd.to_datetime(max_time)
+            
+            # Filter the DataFrame to include only new records
+            filtered_df = df[df[time_column] > max_time]
+            logger.info(f"Filtered data contains {len(filtered_df)} new records to upload.")
+        
+        if filtered_df.empty:
+            logger.info("No new data to upload.")
+            return
+        
+    except Exception as e:
+        logger.error(f"Error querying BigQuery table for max {time_column}: {e}")
+        return
+    
+    # Define BigQuery load job configuration
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_APPEND, 
+        schema=get_bq_schema_from_df(filtered_df),
+        source_format=bigquery.SourceFormat.PARQUET,
+    )
+    
+    try:
+        # Load the filtered DataFrame into BigQuery
+        job = bigquery_client.load_table_from_dataframe(filtered_df, table_ref, job_config=job_config)
+        job.result()  # Wait for the job to complete
+        logger.info(f"Successfully uploaded {len(filtered_df)} records to {bigquery_table_id}")
     
     except BadRequest as e:
         logger.error(f"BadRequest error: {e.message}")
         logger.error(f"Details: {e.errors}")
         
-    
     except Exception as e:
         logger.error(f"An unexpected error occurred while uploading to BigQuery: {e}")
 
@@ -99,7 +137,6 @@ def list_s3_files(prefix: str = '') -> List[str]:
     """
     Lists files in an S3 bucket under a specified prefix and returns the list of file keys.
 
-   
     :param prefix: The prefix under which to list files.
     :return: A list of file keys.
     """
@@ -113,35 +150,38 @@ def list_s3_files(prefix: str = '') -> List[str]:
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
     list_files = []
     if 'Contents' in response:
-        print(f"Files in {bucket_name}/{prefix}:")
+        logger = get_run_logger()
+        logger.info(f"Files in {bucket_name}/{prefix}:")
         for obj in response['Contents']:
-            print(obj['Key'])
+            logger.info(obj['Key'])
             list_files.append(obj['Key'])
     else:
-        print(f"No files found in {bucket_name}/{prefix}.")
+        logger = get_run_logger()
+        logger.info(f"No files found in {bucket_name}/{prefix}.")
 
     return list_files
 
 @flow(log_prints=True, name="s3_to_bigquery", flow_run_name=generate_flow_name)
-def s3_to_bigquery_flow(prefix: str, bigquery_table_id: str, expiration_time: int) -> None:
+def s3_to_bigquery_flow(prefix: str, bigquery_table_id: str, time_column: str, expiration_time: int) -> None:
     """
-    Transfer flow that lists all files in the S3 bucket under a prefix and uploads them to BigQuery.
+    Transfer flow that lists all files in the S3 bucket under a prefix and uploads new data to BigQuery.
 
     :param prefix: The prefix under which to list files.
     :param bigquery_table_id: The BigQuery table ID where data will be uploaded.
+    :param time_column: The column name to determine data uniqueness.
     :param expiration_time: Expiration time in days for the BigQuery table.
     """
     logger = get_run_logger()
     
-    
-    s3_keys = list_s3_files(prefix)
 
+    s3_keys = list_s3_files(prefix)
     
     for s3_key in s3_keys:
         logger.info(f"Processing file: {s3_key}")
         upload_parquet_to_bigquery(
             s3_key=s3_key, 
             bigquery_table_id=bigquery_table_id,
+            time_column=time_column,
             expiration_time=expiration_time
         )
 
